986bdc56cd7efbdcea592c7fac584a213c75f866
wangximing <wangximing@cryptape.com>
2021-10-08 14:34:13 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// File: ./src/App.js
 
-function App() {
+import React from "react"
+import {AuthCluster} from "./auth-cluster"
+import {InitCluster} from "./init-cluster"
+import {useCurrentUser} from "./hooks/current-user"
+
+export default function App() {
+  const cu = useCurrentUser()
+  console.log(cu.loggedIn)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthCluster />
+      <InitCluster address={cu.addr} />
     </div>
-  );
+  )
 }
-
-export default App;
